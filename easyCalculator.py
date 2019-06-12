ceb2489cf86cc3529a5a4a0cef7e9fe6d391d1e3
pkongyang <pakkinee.k@gmail.com>
2019-06-12 14:12:47 +1000

    textbox app

--- easyCalculator.py
+++ easyCalculator.py
@@ -12,28 +12,32 @@ def result():
     messagebox.showinfo('Result', text1+text2+textshow)
 
 
-
 GUI = Tk()
 GUI.geometry('500x300+100+100')
 GUI.title('Calculator')
 
 #Button1
-BCal = Button(GUI,text='Plus(+)')
-BCal.pack()
+#BCal = Button(GUI,text='Plus(+)')
+#BCal.pack(pady=20)
 
+#Text
+Note1 = ttk.Label(GUI, text = 'Input Number 1 :')
+Note1.pack(pady=10)
 #Text box for value entry
 Box1 = StringVar()
 Input1 = ttk.Entry(GUI,textvariable = Box1, font=('Arial',20))
-Input1.pack()
+Input1.pack(pady=20)
 
-
+#Text
+Note2 = ttk.Label(GUI, text = 'Input Number 2 :')
+Note2.pack(pady=10)
 #Text box for value entry
 Box2 = StringVar()
 Input2 = ttk.Entry(GUI,textvariable = Box2, font=('Arial',20))
-Input2.pack()
+Input2.pack(pady=20)
 
 #Button2
-BCal2 = ttk.Button(GUI,text='New Calculate',command=result)
+BCal2 = ttk.Button(GUI,text='Plus Result',command=result)
 BCal2.pack(ipadx = 20, ipady = 10)
 
 
