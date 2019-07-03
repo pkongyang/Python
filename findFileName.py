cc669b45488bc30c821b3d7e1b9c1ee543b3d6d4
pkongyang <pakkinee.k@gmail.com>
2019-07-03 13:40:52 +1000

    Find file by name

--- findFileName.py
+++ findFileName.py
@@ -2,9 +2,9 @@
 
 import glob
 
-path = 'T:\\1. ST CCTV & EES Upgrade Project\\'       #The folder path to find the file
+path = 'T:\\PB MISC\\'       #The folder path to find the file
 
-files = [f for f in glob.glob(path + "**/*town*", recursive=True)]       #the file type can be change
+files = [f for f in glob.glob(path + "**/*circular*", recursive=True)]       #the file type can be change
 
 A = open("file.txt","w+")     #The name of the text file can be change
 
